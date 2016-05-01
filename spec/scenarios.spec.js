d4ed0fe8fb31028d92fc4a473a4de8742fb71f7c
Florent Le Gall <florent.legall@gmail.com>
2016-05-01 11:35:44 +0200

    refactor(tests): Use scenarios DSL

--- spec/scenarios.spec.js
+++ spec/scenarios.spec.js
@@ -1,29 +1,34 @@
+import {scenarios, setupForRspec} from '../index';
 import {ScenarioRunner} from '../src/scenarios';
 
 import {expect} from 'chai';
 import sinon from 'sinon';
 
-describe('ScenarioRunner', () => {
-    it('should be able to run scenarios over an rspec runner', () => {
-        // given
-        const scenarioRunner = new ScenarioRunner();
-        const describe = sinon.stub();
-        const it = sinon.stub();
-        scenarioRunner.setupForRspec(describe, it);
-        const scenarioFunc = sinon.spy();
+setupForRspec(describe, it);
 
-        // when
-        scenarioRunner.scenarios('groupName', () => {
-            return {
-                my_scenario_name: scenarioFunc
-            };
-        });
-        describe.callArg(1); // Emulate rspec describe()
-        it.callArg(1); // Emulate rspec it()
+scenarios('ScenarioRunner', () => {
+    return {
+        should_be_able_to_run_scenarios_over_an_rspec_runner() {
+            // given
+            const scenarioRunner = new ScenarioRunner();
+            const describe = sinon.stub();
+            const it = sinon.stub();
+            scenarioRunner.setupForRspec(describe, it);
+            const scenarioFunc = sinon.spy();
 
-        // then
-        expect(describe).to.have.been.calledWith('groupName');
-        expect(it).to.have.been.calledWithExactly('my_scenario_name', scenarioFunc);
-        expect(scenarioFunc).to.have.been.called;
-    });
+            // when
+            scenarioRunner.scenarios('groupName', () => {
+                return {
+                    my_scenario_name: scenarioFunc
+                };
+            });
+            describe.callArg(1); // Emulate rspec describe()
+            it.callArg(1); // Emulate rspec it()
+
+            // then
+            expect(describe).to.have.been.calledWith('groupName');
+            expect(it).to.have.been.calledWithExactly('my_scenario_name', scenarioFunc);
+            expect(scenarioFunc).to.have.been.called;
+        }
+    };
 });
