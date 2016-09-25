6f87b4c7d179f3f300280a00ae17a4a6ba9de06b
Florent Le Gall <florent.legall@gmail.com>
2016-09-25 10:29:24 +0200

    Build a scenario model

--- src/generateReport.js
+++ src/generateReport.js
@@ -2,11 +2,13 @@
 import fs from 'fs';
 import zlib from 'zlib';
 
+import _ from 'lodash';
 import maven from 'maven';
 import rimraf from 'rimraf';
 import DecompressZip from 'decompress-zip';
 
 import {REPORTS_DESTINATION, ScenarioReport} from './reports';
+import type {ScenarioModel} from './jgivenReport/ScenarioModel';
 
 export const JGIVEN_APP_VERSION = '0.12.1';
 
@@ -45,10 +47,14 @@ export function generateJGivenReportDataFiles(filter?: (fileName: string) => boo
     const files = fs.readdirSync(`./${REPORTS_DESTINATION}`).filter(filter);
     const scenarioReports: ScenarioReport[] = files.map(file =>
         JSON.parse(fs.readFileSync(`${REPORTS_DESTINATION}/${file}`, 'utf-8')));
+    const groupNamesAndScenarios: Array<[string, ScenarioReport[]]> =
+        (Object.entries(_.groupBy(scenarioReports, ({groupReport: {name}}) => name)): any);
+    const scenarioModels = groupNamesAndScenarios.map(([groupName, scenarioReports]) =>
+        toScenarioModel(groupName, scenarioReports));
 
     fs.writeFileSync('./jGiven-report/data/metaData.js', `jgivenReport.setMetaData({"created":"${new Date().toLocaleString()}","title":"J(s)Given Report","data":["data0.js"]} );\n`, 'utf-8');
 
-    const json = JSON.stringify(scenarioReports);
+    const json = JSON.stringify(scenarioModels);
     const buffer = zlib.gzipSync(new Buffer(json, 'utf-8'));
     const base64 = buffer.toString('base64');
     fs.writeFileSync('./jGiven-report/data/data0.js', `jgivenReport.addZippedScenarios('${base64}');\n`, 'utf-8');
@@ -84,4 +90,20 @@ function unzip(zipFile: string, targetDirectory: string): Promise<void> {
             path: targetDirectory,
         });
     });
+}
+
+function toScenarioModel(groupName: string, scenarioReports: ScenarioReport[]): ScenarioModel {
+    return {
+        executionStatus: 'SUCCESS',
+        casesAsTable: false,
+        derivedParameters: [],
+        description: '',
+        durationInNanos: 42,
+        explicitParameters: [],
+        extendedDescription: '',
+        tagIds: [],
+        className: groupName,
+        scenarioCases: [],
+        testMethodName: groupName,
+    };
 }
