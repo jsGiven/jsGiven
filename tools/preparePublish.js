e883480e5bf0702710f1218ae0752298576e6ae0
Florent Le Gall <florent.legall@gmail.com>
2016-10-09 15:34:03 +0200

    Handle automatically publishing of alpha

--- tools/preparePublish.js
+++ tools/preparePublish.js
@@ -11,6 +11,16 @@ pkg.scripts = {
     postinstall: 'node installJGivenReport.js',
 };
 
+var isAlpha = false;
+process.argv.forEach(function (val) {
+    if (val === 'alpha') {
+        isAlpha = true;
+    }
+});
+if (isAlpha) {
+    pkg.version = pkg.__versionAlpha;
+}
+
 fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
 fs.writeFileSync('dist/LICENSE', fs.readFileSync('LICENSE', 'utf-8'), 'utf-8');
 fs.writeFileSync('dist/README.md', fs.readFileSync('README.md', 'utf-8'), 'utf-8');
