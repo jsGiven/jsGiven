acc1139a92c90b4c8fedd6fe4c62c166a7b2a0fe
Florent Le Gall <florent.legall@gmail.com>
2017-05-17 12:27:06 +0200

    Add flow type definition

--- js-given/src/index.js
+++ js-given/src/index.js
@@ -1,5 +1,16 @@
 // @flow
 import 'babel-polyfill';
-export {parametrized1, parametrized2, scenarios, State} from './scenarios';
+export {
+    parametrized,
+    parametrized1,
+    parametrized2,
+    parametrized3,
+    parametrized4,
+    parametrized5,
+    parametrized6,
+    parametrized7,
+    scenarios,
+    State,
+} from './scenarios';
 export {setupForRspec, setupForAva} from './test-runners';
 export {Stage} from './Stage';
