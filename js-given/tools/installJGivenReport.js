dad3413a569951e9f27d4c77d06cd15cf2f6e7a9
Florent Le Gall <florent.legall@gmail.com>
2017-05-21 23:34:24 +0200

    fix(jgiven): Download jgiven with https and fixes vulnerability discovered by snyk : https://snyk.io/vuln/npm:js-given

--- js-given/tools/installJGivenReport.js
+++ js-given/tools/installJGivenReport.js
@@ -1,17 +1,17 @@
 // @flow
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
 
 var jGivenVersion = '0.12.1';
 
 var jgivenFile = 'jgiven-html5-report.jar';
-var jgivenURL = 'http://central.maven.org/maven2/com/tngtech/jgiven/jgiven-html5-report/' + jGivenVersion + '/jgiven-html5-report-' + jGivenVersion + '.jar';
+var jgivenURL = 'https://repo1.maven.org/maven2/com/tngtech/jgiven/jgiven-html5-report/' + jGivenVersion + '/jgiven-html5-report-' + jGivenVersion + '.jar';
 
 console.log('Downloading JGiven ' + jGivenVersion);
 
 var file = fs.createWriteStream(jgivenFile);
 // eslint-disable-next-line flowtype/require-parameter-type
-http.get(jgivenURL, function (response/*: any*/) {
+https.get(jgivenURL, function (response/*: any*/) {
     if (response.statusCode >= 400) {
         throw new Error('Invalid response : received a ' + response.statusCode + ' error for ' + jgivenURL);
     }
