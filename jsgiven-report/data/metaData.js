8b20739bdb2a6d61f089b46729af6ea3bbfe4d7a
Florent Le Gall <florent.legall@fluo.com>
2017-05-25 22:32:51 +0200

    Updates

--- jsgiven-report/data/metaData.js
+++ jsgiven-report/data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"5/25/2017, 8:44:17 PM","title":"J(s)Given Report","data":["data0.js"]} );
+jgivenReport.setMetaData({"created":"5/25/2017, 10:32:48 PM","title":"J(s)Given Report","data":["data0.js"]} );
