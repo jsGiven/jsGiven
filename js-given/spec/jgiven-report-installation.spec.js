bfa1cea2cd27d3e0d3e417d588e29446faaa45e8
Florent Le Gall <florent.legall@gmail.com>
2017-06-01 11:32:17 +0200

    refactor(specs): Rewrite jgiven report installation test with jsgiven

--- js-given/spec/jgiven-report-installation.spec.js
+++ js-given/spec/jgiven-report-installation.spec.js
@@ -4,46 +4,58 @@ import fs from 'fs';
 import tmp from 'tmp';
 import {expect} from 'chai';
 
+import {scenarios, setupForRspec, setupForAva, Stage, doAsync} from '../src';
+import {installJGivenReportApp} from '../src/generateJGivenReport';
+
 if (global.jasmine) {
     global.jasmine.DEFAULT_TIMEOUT_INTERVAL = 1800000;
 }
-import {installJGivenReportApp} from '../src/generateJGivenReport';
 
-// Todo implement jsGiven test once it supports async tests
 if (global.describe && global.it) {
-    if (global.TEST_FRAMEWORK === 'JASMINE') {
-        describe('JGiven report', () => {
-            it('should install the report app', async (done) => {
-                await testInstallJGivenReportApp();
-                if (done) {
-                    done();
-                }
-            });
-        });
-    } else {
-        describe('JGiven report', () => {
-            it('should install the report app', async () => {
-                await testInstallJGivenReportApp();
-            });
-        });
-    }
+    setupForRspec(describe, it);
 } else {
     const test = require('ava');
-    test('JGiven report should install the report app', async () => {
-        await testInstallJGivenReportApp();
-    });
+    setupForAva(test);
 }
 
-async function testInstallJGivenReportApp(): Promise<void> {
-    const tmpDir = tmp.dirSync({unsafeCleanup: true});
-    const reportDir = tmpDir.name;
+class JGivenReportInstallationStage extends Stage {
+    reportDir: string;
 
-    await installJGivenReportApp(tmpDir.name);
+    a_temp_directory(): this {
+        const tmpDir = tmp.dirSync({unsafeCleanup: true});
+        this.reportDir = tmpDir.name;
 
-    expect(fs.existsSync(`${reportDir}/jGiven-report`)).to.be.true;
-    expect(fs.existsSync(`${reportDir}/jGiven-report/index.html`)).to.be.true;
-    expect(fs.existsSync(`${reportDir}/jGiven-report/data`)).to.be.true;
-    expect(fs.existsSync(`${reportDir}/jGiven-report/META-INF`)).to.be.false;
-    expect(fs.existsSync(`${reportDir}/jGiven-report/com`)).to.be.false;
-    expect(fs.existsSync(`${reportDir}/jGiven-report/jgiven-html5-report.jar`)).to.be.false;
+        return this;
+    }
+
+    the_jgiven_report_is_installed(): this {
+        doAsync(async () => {
+            await installJGivenReportApp(this.reportDir);
+        });
+
+        return this;
+    }
+
+    the_report_has_been_installed(): this {
+        const {reportDir} = this;
+        expect(fs.existsSync(`${reportDir}/jGiven-report`)).to.be.true;
+        expect(fs.existsSync(`${reportDir}/jGiven-report/index.html`)).to.be.true;
+        expect(fs.existsSync(`${reportDir}/jGiven-report/data`)).to.be.true;
+        expect(fs.existsSync(`${reportDir}/jGiven-report/META-INF`)).to.be.false;
+        expect(fs.existsSync(`${reportDir}/jGiven-report/com`)).to.be.false;
+        expect(fs.existsSync(`${reportDir}/jGiven-report/jgiven-html5-report.jar`)).to.be.false;
+        return this;
+    }
 }
+
+scenarios('core.reports.jgiven.installation', JGivenReportInstallationStage, ({given, when, then}) => {
+    return {
+        a_simple_report_is_generated() {
+            given().a_temp_directory();
+
+            when().the_jgiven_report_is_installed();
+
+            then().the_report_has_been_installed();
+        },
+    };
+});
