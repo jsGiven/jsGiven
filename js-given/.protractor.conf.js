8b88085a5b5adf323da94f9e16ab7b5ec723018f
Florent Le Gall <florent.legall@gmail.com>
2017-06-12 22:29:03 +0200

    chore(protractor): Use latest protractor version

--- js-given/.protractor.conf.js
+++ js-given/.protractor.conf.js
@@ -1,11 +1,14 @@
 // Choosing firefox on CI, Chrome at home
 var os = require('os');
-var browser = os.platform() === 'linux' ? 'firefox' : 'chrome';
+var capabilities = os.platform() === 'linux' ? {
+    browserName: 'firefox',
+    marionette: false
+} : {
+    browserName: 'chrome',
+};
 
 exports.config = {
-    capabilities: {
-        'browserName': browser,
-    },
+    capabilities: capabilities,
     specs: ['spec/**.spec.js'],
     onPrepare: function () {
         require("babel-core/register");
