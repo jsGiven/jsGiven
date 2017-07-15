9574c490053dd2d8f62ebe7a39457eed4f2b456d
Florent Le Gall <florent.legall@gmail.com>
2017-07-15 22:42:03 +0200

    chore(flow): Bump flow version

--- js-given/spec/support/jasmine-runner.js
+++ js-given/spec/support/jasmine-runner.js
@@ -4,10 +4,9 @@ var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 var jrunner = new Jasmine();
 
+/*eslint-disable */
 // $FlowIgnore
-/*eslint-disable */
 jasmine.getEnv().clearReporters(); // add jasmine-spec-reporter
-/*eslint-disable */
 jasmine.getEnv().addReporter(new SpecReporter()); // add jasmine-spec-reporter
 /*eslint-enable */
 jrunner.loadConfigFile(); // load jasmine.json configuration
