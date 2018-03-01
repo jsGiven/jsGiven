35a959118f56dc23e97369fbb1da9cac5b6d4768
Florent Le Gall <florent.legall@orange.com>
2018-03-01 09:32:58 +0100

    Updates

--- jsgiven-report/data/metaData.js
+++ jsgiven-report/data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"2018-3-1 09:28:58","title":"J(s)Given Report","data":["data0.js"]} );
+jgivenReport.setMetaData({"created":"2018-3-1 09:32:56","title":"J(s)Given Report","data":["data0.js"]} );
